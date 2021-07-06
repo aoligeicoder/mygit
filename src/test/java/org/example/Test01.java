9d19d9bbdb4f6861d3857a15b6efda3df9562072
yyb <2419087018@qq.com>
2021-07-06 12:52:59 +0800

    新加test01

--- src/test/java/org/example/Test01.java
+++ src/test/java/org/example/Test01.java
@@ -3,5 +3,7 @@ package org.example;
 public class Test01 {
     public static void main(String[] args) {
         System.out.println("gan!!!!");
+        System.out.println("gan!!!!");
+        System.out.println("gan!!!!");
     }
 }
